ef9b20264d62b4545d247d2f8735556cccc05a33
Erlinda <erlindachambimanzanoerlinda@gamil.com>
2020-04-06 17:46:33 -0400

    pagina con componentes

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -1,8 +1,35 @@
 import React from 'react';
 
-class BadgeNew extends React.Component{
-    render(){
-        return <h1>Badge New</h1>
+import header from '../images/badge-header.svg';
+//import './styles/BadgeNew.css'
+
+import Navbar from '../components/Navbar'
+import Badge from '../components/Badge'
+
+class BadgeNew extends React.Component {
+    render() {
+        return (
+            <div>
+                <Navbar />
+
+                <div className="BadgeNew__hero">
+                    <img className="img-fluid" src={header} alt="Logo"></img>
+                </div>
+                <div className="contanier">
+                    <div className="row">
+                        <div className="col">
+                            <Badge
+                                firstName="Erlinda"
+                                lastName="Chambi"
+                                twitter="erlindachambi"
+                                jobTitle="Frontend Engineer" 
+                                avatar="https://www.gravatar.com/avatar?d=identicon"
+                                />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
     }
 }
 
