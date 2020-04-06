04bad964c496622d6698c3aa2100a582a36ac036
Erlinda <erlindachambimanzanoerlinda@gamil.com>
2020-04-06 17:14:36 -0400

    una page

--- src/index.js
+++ src/index.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Badge from './components/Badge';
 import 'bootstrap/dist/css/bootstrap.css';
 import './global.css';
+
+import BadgeNew from './pages/BadgeNew';
+
 
 
 const contanier = document.getElementById('app');  //app es el id de un div
 
-ReactDOM.render(
-    <Badge
-        firstName="Erlinda"
-        lastName="Chambi"
-        avatar="https://www.gravatar.com/avatar?d=identicon"
-        jobTitle="Fronted Engineer"
-        twitter="lindachambi" 
-        />,
-    contanier
-);
+ReactDOM.render(<BadgeNew />,  contanier);
