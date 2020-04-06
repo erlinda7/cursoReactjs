7ed4ac3586272b2671e1dec5b81b4ff79a0145ce
Erlinda <erlindachambimanzanoerlinda@gamil.com>
2020-04-06 16:56:32 -0400

    componentes con estilos

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,10 +1,34 @@
 import React from 'react';
 
-class Badge extends React.Component{
+import confLogo from '../images/badge-header.svg'
+import './styles/Badge.css'; //react-app y webpack saben como insertarlo
 
-    render(){
-        return(
-            <h1>Badge</h1>
+class Badge extends React.Component {
+
+    render() {
+        const firstName = 'Erlinda';
+        const lastName = 'Chambi';
+        return (
+            <div className="Badge">
+                <div className="Badge__header">
+                    <img src={confLogo} alt="Logo de la conferencia" />
+                </div>
+
+                <div className="Badge__section-name">
+                    <img className="Badge__avatar" src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar"></img>
+                    <h1>{firstName} <br />{lastName}</h1>
+                </div>
+
+                <div className="Badge__section-info">
+                    <p>Fronted Engineer</p>
+                    <div>@sparragus</div>
+                </div>
+
+                <div className="Badge__footer">
+                    #plazticonf
+                </div>
+
+            </div>
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Badge from './components/Badge'
+import Badge from './components/Badge';
+import 'bootstrap/dist/css/bootstrap.css';
+import './global.css';
 
 
 const contanier = document.getElementById('app');  //app es el id de un div
 
-ReactDOM.render(<Badge/>, contanier);
+ReactDOM.render(<Badge />, contanier);
