a986011fab2ba9bdc060afc36c2b6858a86ed5f4
Erlinda <erlindachambimanzanoerlinda@gamil.com>
2020-04-07 17:49:20 -0400

    pagina prinicapl Home

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import Layout from './Layout';
 import BadgeNew from '../pages/BadgeNew';
 import Badges from '../pages/Badges';
 import NotFound from '../pages/NotFound';
+import Home from '../pages/Home';
 
 //componente sin state
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <BrowserRouter>
             <Layout>
                 <Switch>
+                    <Route exact path="/" component={Home} />
                     <Route exact path="/badges" component={Badges} />
                     <Route exact path="/badges/new" component={BadgeNew} />
                     <Route component={NotFound}></Route>
